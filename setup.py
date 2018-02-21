36e499a0f45e433ea5223735ca448ecc0677440f
Jesse <je55ek@users.noreply.github.com>
2018-02-20 22:45:40 -0600

    Revd version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
       name        = 'wedding',
-      version     = '1.0.0',
+      version     = '1.0.16',
       description = 'Flying Js serverless wedding RSVP application.',
       author      = 'Jesse',
       license     = 'MIT',
